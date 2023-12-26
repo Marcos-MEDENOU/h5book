96ef0442d70f8a88b9e940220930da5b557251e0
Kola771 <koladeaboudou@gmail.com>
2023-12-26 12:14:40 +0100

    first

--- resources/js/Pages/Users/Friends.vue
+++ resources/js/Pages/Users/Friends.vue
@@ -10,17 +10,18 @@ import { Head } from '@inertiajs/vue3';
 
     <main>
         <section>
-            <div class="bg-url(['/storage/images/persona_smal.png'])">
-dd
+            <div class="bg-[url('/storage/images/profile.jpg')] bg-center bg-no-repeat bg-cover h-[150px]">
             </div>
+            <button class="bgColor border-none colorblue font-bold">Modifier l'arrière-plan</button>
         </section>
     </main>
 </template>
 
-<style scope>.color {
-    color: #f8f9fa;
+<style scope>
+.bgColor {
+    background-color: #f8f9fa;
 }
-
-.headerBg {
-    background-color: #0389c9;
-}</style>
+.colorblue {
+    color: #0389c9
+}
+</style>
