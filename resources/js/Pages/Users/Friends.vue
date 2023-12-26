082f69fe01e111f5800f8fda1f537179dbbe508d
Kola771 <koladeaboudou@gmail.com>
2023-12-26 11:53:39 +0100

    first

---
+++ resources/js/Pages/Users/Friends.vue
@@ -0,0 +1,90 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import Navbar from '@/Layouts/Navbar.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Friends" />
+    <header class="headerBg py-2 sticky top-0">
+        <div class="mx-auto w-[90%] flex justify-between items-center">
+            <div class="basis-[70%] flex items-center gap-4">
+                <div class="relative basis-[20%]">
+                    <span class="absolute w-[24px] bg-white opacity-50 color rounded-lg px-2">H5</span>
+                    <h1 class="color text-[18px] font-bold">
+                        H5book
+                    </h1>
+                </div>
+                <form class="basis-[70%] flex items-center gap-4 py-1 pl-2 pr-1 border border-sky-500 rounded-lg">
+                    <div class="">
+                        <svg class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                        </svg>
+                    </div>
+                    <input type="text"
+                        class="w-full headerBg py-1 rounded-lg outline-none border-none focus:border-none focus:border-sky-600 color"
+                        placeholder="Recherchez un proche...">
+                </form>
+            </div>
+            <div class="basis-[20%]">
+                <ul class="flex gap-4 justify-end items-center">
+                    <li v-if="bool">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none"
+                            stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                            class="cursor-pointer feather feather-sun icon-light stroke-width-3 iw-16 ih-16">
+                            <circle cx="12" cy="12" r="5"></circle>
+                            <line x1="12" y1="1" x2="12" y2="3"></line>
+                            <line x1="12" y1="21" x2="12" y2="23"></line>
+                            <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
+                            <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
+                            <line x1="1" y1="12" x2="3" y2="12"></line>
+                            <line x1="21" y1="12" x2="23" y2="12"></line>
+                            <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
+                            <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
+                        </svg>
+                    </li>
+                    <li v-else>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none"
+                            stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                            class="cursor-pointer feather feather-moon icon-light stroke-width-3 iw-16 ih-16">
+                            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+                        </svg>
+                    </li>
+                    <li>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none"
+                            stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                            class="cursor-pointer feather feather-grid icon-light stroke-width-3 iw-16 ih-16">
+                            <rect x="3" y="3" width="7" height="7"></rect>
+                            <rect x="14" y="3" width="7" height="7"></rect>
+                            <rect x="14" y="14" width="7" height="7"></rect>
+                            <rect x="3" y="14" width="7" height="7"></rect>
+                        </svg>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <!-- <AuthenticatedLayout> -->
+<!-- <template #header> -->
+<!-- <h2 class="font-semibold text-xl text-gray-800 leading-tight">Accueil</h2> -->
+<!-- </template> -->
+
+<!-- <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">Vous êtes connecter!</div>
+                </div>
+            </div>
+        </div> -->
+<!-- </AuthenticatedLayout> -->
+</template>
+
+<style scope>.color {
+    color: #f8f9fa;
+}
+
+.headerBg {
+    background-color: #0389c9;
+}</style>
